b97be8e300da715d4815055de28383a5e5c8f165
zuams <apkssky@gmail.com>
2017-11-25 21:00:07 +0700

    Update undang.js

--- undang.js
+++ undang.js
@@ -20,11 +20,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*3)+ 1);
+var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2017/11/4-best-tips-for-choosing-fire-insurance.html"}
 if(x=="2"){xxx="/2017/11/20-ways-to-take-care-of-face.html"}
-if(x=="3"){xxx="/2017/11/car-production-fall-hits-uk-industrial.html"}a_to_ve[i].href="https://theratsway.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/2017/11/car-production-fall-hits-uk-industrial.html"}
+if(x=="4"){xxx="/2017/11/should-you-pay-off-your-student-loans.html"}
+if(x=="5"){xxx="/2017/11/opportunities-to-increase-your-income.html"}a_to_ve[i].href="https://theratsway.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
